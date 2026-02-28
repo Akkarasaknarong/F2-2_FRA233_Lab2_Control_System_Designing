52aabdf3c636408dd1f3b01764a55a0e1015ee2b
RBH <159563081+Akkarasaknarong@users.noreply.github.com>
2026-02-28 19:36:20 +0700

    Upload Pboon Zip

---
+++ FRA233_Lab2_Control_System_Designing-main/Lab2_params_student.m
@@ -0,0 +1,12 @@
+% Pendulum Param
+L = 0.1;     % [m]  (massless link)
+mp = 0.05;   % [kg] (point mass)
+g = 9.81;    % [m/s^2]
+% Motor Param from experiment
+kt = 0.05;
+ke = 0.05;
+Lm = 4e-3;
+R = 3;
+b = 1e-4;
+J = 7e-5;
+
