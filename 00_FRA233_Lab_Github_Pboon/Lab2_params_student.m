30e71cc70fceb2dacfe821728a4fbbce3baae047
RBH <159563081+Akkarasaknarong@users.noreply.github.com>
2026-03-18 02:17:21 +0700

    Upload

--- 00_FRA233_Lab_Github_Pboon/Lab2_params_student.m
+++ 00_FRA233_Lab_Github_Pboon/Lab2_params_student.m
@@ -3,20 +3,20 @@ L = 0.1;     % [m]  (massless link)
 mp = 0.05;   % [kg] (point mass)
 g = 9.81;    % [m/s^2]
 % Motor Param from experiment [TA Parameter]
-R = 3.18;
-Lm = 2.8445e-3;
-b = 77.581e-6;
-J = 58.559e-6;
-kt = 50.6e-3;
-ke = 52.8e-3;
+% R = 3.18;
+% Lm = 2.8445e-3;
+% b = 77.581e-6;
+% J = 58.559e-6;
+% kt = 50.6e-3;
+% ke = 52.8e-3;
 
 % Motor Param from experiment [G8 Parameter]
-% R = 3.57;
-% Lm = 3.313e-3;
-% b = 2.14e-6;
-% J = 11.739e-6;
-% kt = 49.575e-3;
-% ke = 50.668e-3;
+R = 3.57;
+Lm = 3.313e-3;
+b = 2.14e-6;
+J = 11.739e-6;
+kt = 49.575e-3;
+ke = 50.668e-3;
 
 % ke = 0.06974;    % Back EMF constant (V/(rad/s))
 % kt = 0.0302;     % Torque constant (N.m/A)
@@ -26,19 +26,20 @@ ke = 52.8e-3;
 % b  = 0.00000835; % Viscous friction coefficient
 
 Tau = Lm/R;
-% Tau = 4e-04 ;
 
-% Kp = 1.5;
-
-% PID Control
+% PID Control (TA Parameter)
 % Kp = 0.5503680603820997 ;
 % Ki = 1.355252715606881e-20 ;
 % Kd = 0.2114551435512229 ;
 
+% PID Control (G8 Parameter)
+Kp = 0.5503680603820997 ;
+Ki = 1.355252715606881e-20 ;
+Kd = 0.2114551435512229 ;
+
 % Contol system design setup
 % J_eq = J + (mp*L*L);
 % num = [kt] ;
-% den = [J_eq*Lm , J_eq*R + Lm*b , R*b + kt*ke , 0];
-% 
+% den = [J_eq*Lm , J_eq*R + Lm*b , R*b + kt*ke , kt];
 % G = tf(num,den);
 % controlSystemDesigner('rlocus',G);
---
+++ 01_Data_Acquisition_Tool/Plot_Difference_Tau.m
@@ -0,0 +1,42 @@
+clear; clc; close all;
+
+% Load file 
+data1 = 'C:\Users\Akkarasaknarong\Documents\GitHub\F2-2_FRA233_Lab2_Control_System_Designing\02_Raw_Experimental_Data\Part_2\Part_2.2\Difference_Tau\1e-2.mat';
+data2 = 'C:\Users\Akkarasaknarong\Documents\GitHub\F2-2_FRA233_Lab2_Control_System_Designing\02_Raw_Experimental_Data\Part_2\Part_2.2\Difference_Tau\1e-3.mat';
+data3 = 'C:\Users\Akkarasaknarong\Documents\GitHub\F2-2_FRA233_Lab2_Control_System_Designing\02_Raw_Experimental_Data\Part_2\Part_2.2\Difference_Tau\1e-4.mat';
+
+% Set .mat file
+f1 = load(data1);
+d1 = f1.data;
+time_1 = squeeze(d1{2}.Values.Time);
+velo_1 = squeeze(d1{2}.Values.Data);
+
+f2 = load(data2);
+d2 = f2.data;
+time_2 = squeeze(d2{2}.Values.Time);
+velo_2 = squeeze(d2{2}.Values.Data);
+
+f3 = load(data3);
+d3 = f3.data;
+time_3 = squeeze(d3{2}.Values.Time);
+velo_3 = squeeze(d3{2}.Values.Data);
+
+
+% Plot
+figure('Color','w', 'Position', [100, 100, 1200, 600])
+hold on
+grid on
+
+plot(time_1, velo_1, 'r', 'LineWidth', 1.5)
+plot(time_2, velo_2, 'g', 'LineWidth', 1.5)
+plot(time_3, velo_3, 'b', 'LineWidth', 1.5)
+
+xlabel('Time [s]', 'FontWeight', 'bold')
+ylabel('Position [rad/s]', 'FontWeight', 'bold')
+title('Response Gravity Compensation Difference (Tau)')
+
+legend('Tau = 1e-02', 'Tau = 1e-03', 'Tau = 1e-04')
+set(gca, 'FontSize', 12)
+
+xlim([0.11 15]); 
+% ylim([50 100]);
