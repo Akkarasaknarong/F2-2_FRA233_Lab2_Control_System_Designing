167c1b5360bb053fa7dde9e8faf2d63d3dd19e42
RBH <159563081+Akkarasaknarong@users.noreply.github.com>
2026-03-18 14:23:24 +0700

    Upload

--- 00_FRA233_Lab_Github_Pboon/Lab2_params_student.m
+++ 00_FRA233_Lab_Github_Pboon/Lab2_params_student.m
@@ -13,36 +13,28 @@ g = 9.81;    % [m/s^2]
 % ke = 52.8e-3;
 
 % Motor Param from experiment [G8 Parameter]
-% R = 3.57;
-% Lm = 3.313e-3;
-% b = 2.14e-6;
-% J = 11.739e-6;
-% kt = 49.575e-3;
-% ke = 50.668e-3;
-
-ke = 0.06974;    % Back EMF constant (V/(rad/s))
-kt = 0.0302;     % Torque constant (N.m/A)
-R  = 2.736;      % Armature resistance (Ohm)
-Lm  = 0.00296;   % Armature inductance (H)
-J  = 0.00001474; % Rotor inertia (kg.m^2)
-b  = 0.00000835; % Viscous friction coefficient
-
+R = 3.57;
+Lm = 3.313e-3;
+b = 2.14e-6;
+J = 11.739e-6;
+kt = 49.575e-3;
+ke = 50.668e-3;
 Tau = Lm/R;
 
 sampling_time = 0.001;
 %% PID Selection
 % Position_Contol Single loop (G8 Parameter)
-Kp = 0.5503680603820997 ;
-Ki = 1.355252715606881e-20 ;
+% Kp = 0.5503680603820997 ;
+% Ki = 1.355252715606881e-20 ;
 % Kd = 0.2114551435512229 ;
 
-% Cascade_Control TEST
-% Kp_Posi = 0.55037;
-% Ki_Posi = 1.3553e-20;
-% Kd_Posi = 0.21146;
-% Kp_Velo = 12.284;
-% Ki_Velo = 1.3553e-20;
-% Kd_Velo = 0.25018;
+% Cascade_Control G8
+Kp_Posi = 0.5503680603820997;
+Ki_Posi = 1.355252715606881e-20;
+Kd_Posi = 0.2114551435512229;
+Kp_Velo = 12.284;
+Ki_Velo = 1.3553e-20;
+Kd_Velo = 0.25018;
 
 
 %% Contol system design setup
